5145c6a8940f08e4e1b1afa63881e534966167f0
Administrator <user@email.com>
2020-01-23 13:57:11 +0800

    init

--- src/main/java/com/github/makewheels/HttpUtil.java
+++ src/main/java/com/github/makewheels/HttpUtil.java
@@ -1,7 +1,102 @@
 package com.github.makewheels;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
- * @time 2020-01-23 13:46
+ * Http工具类
+ *
+ * @author Administrator
  */
 public class HttpUtil {
+    private static String userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36";
+    private static String contentType = "application/x-www-form-urlencoded";
+
+
+    public static String tryGet(String url) throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet();
+        httpGet.addHeader("User-Agent", userAgent);
+        httpGet.setHeader("Content-type", contentType);
+        httpGet.setHeader("Connection", "keep-alive");
+        System.out.println("HttpClient GET: " + url);
+        httpGet.setURI(URI.create(url));
+        CloseableHttpResponse response = null;
+        response = client.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        return EntityUtils.toString(entity, Charset.defaultCharset());
+    }
+
+    public static String get(String url) {
+        try {
+            return tryGet(url);
+        } catch (Exception e) {
+            System.err.println("http get error: " + e.getMessage() + ", retry " + url);
+            try {
+                return tryGet(url);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 发送post请求
+     *
+     * @param url
+     * @return
+     */
+    public static String post(String url, Map<String, String> param) {
+        System.out.println("HttpClient POST: " + url);
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        // 装填参数
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        if (param != null) {
+            for (Entry<String, String> entry : param.entrySet()) {
+                nvps.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            }
+        }
+        // 设置参数到请求对象中
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        httpPost.setHeader("User-Agent", userAgent);
+        httpPost.setHeader("Content-type", contentType);
+        String body = null;
+        try {
+            CloseableHttpResponse response = client.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                body = EntityUtils.toString(entity, Charset.defaultCharset());
+            }
+            EntityUtils.consume(entity);
+            response.close();
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
+
 }
